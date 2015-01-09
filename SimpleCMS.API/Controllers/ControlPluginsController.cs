1c3f83ece6519f5748c6405662bd0b02bf17c27d
AgronKabashi <agron.kabashi@tretton37.com>
2015-01-09 16:04:33 +0100

    Added ContronPluginsControler

---
+++ SimpleCMS.API/Controllers/ControlPluginsController.cs
@@ -0,0 +1,18 @@
+﻿using Cerberus.Tool.TemplateEngine.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleCMS.API.Controllers
+{
+	public class ControlPluginsController : ApiController
+	{
+		public ControlPluginCollection Get()
+		{
+			return TemplateManager.ControlPluginService.GetControlPlugins();
+		}
+	}
+}
